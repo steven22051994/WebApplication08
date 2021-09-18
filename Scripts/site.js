70ac0959503e391f2629ac31aa13b9383c594b00
Chrissemann94 <90688849+Chrissemann94@users.noreply.github.com>
2021-09-18 12:23:38 +0200

    Skeleton > Bootstrap
    Changed Content, Script Folder and Layout.cshtml

--- Scripts/site.js
+++
@@ -1,8 +0,0 @@
-﻿function openDrawerMenu() {
-    var x = document.getElementById("mainNavBar");
-    if (x.className === "navBar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navBar";
-    }
-}
